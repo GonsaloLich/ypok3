60e93d0c686ca4665249ddf6976cae2ebf7615e1
GonsaloLich <kantsaris@gmail.com>
2023-11-12 15:19:22 +0300

    дз готово

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Author authorViy = new Author("Николай", "Гоголь");
+        Author authorsteelRat = new Author("Гарри", "Гаррисон");
+        Book viy = new Book(authorViy, "Вий", 1835);
+        Book steelRat = new Book(authorsteelRat, "Стальная крыса", 1985);
+        steelRat.setYear(1990);
+
+    }
 }
